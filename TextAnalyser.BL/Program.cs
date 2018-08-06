ac1e053519e0767c2864a99e0fa40df33e1e3cb0
Salvatore85 <salvatore.m@gmail.com>
2018-08-06 16:09:15 +0200

    First commit

---
+++ TextAnalyser.BL/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TextAnalyser.BL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testText = "Hallo dit is een test zin.!!";
+
+            var sample = new TextStatsGenerator();
+
+
+            Console.WriteLine(sample.SpecialCharactersCounter(testText));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TextAnalyser.BL/TextStatsGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TextAnalyser.BL
+{
+    class TextStatsGenerator
+    {
+        public int WordCounter(string txt)
+        {
+            var words = txt.Split(" ");
+
+            return words.Length;
+        }
+
+        public int SpecialCharactersCounter(string txt)
+        {
+            var specialCharacters = Regex.Matches(txt, "[(!, -')*]").Count;
+
+            return specialCharacters;
+        }
+    }
+}
