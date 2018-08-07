ef69b352ec7617bf401013673dcf8eb7a033226e
Salvatore85 <salvatore.m@gmail.com>
2018-08-07 15:42:24 +0200

    Minor improvment

--- TextAnalyser.BL/Program.cs
+++ TextAnalyser.BL/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Authentication.ExtendedProtection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TextAnalyser.BL
@@ -8,9 +7,6 @@ namespace TextAnalyser.BL
     {
         static void Main(string[] args)
         {
-            //DI Setup
-            var serviceProvider = new ServiceCollection();
-
             string testText = "Hallo dit is een test zin.!!";
 
             //var sample = new SortingText();
